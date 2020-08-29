031dfbf0c8eab1a9777d8431d5fd151aa5f81da3
John Boyes <john@agilepathway.co.uk>
2020-08-29 01:38:59 +0000

    Improve documentation for wrapper script

--- scripts/secrethub-wrapper.sh
+++ scripts/secrethub-wrapper.sh
@@ -7,7 +7,7 @@ if [[ -z "${SECRETHUB_USERNAME}" ]]; then
   read -r SECRETHUB_USERNAME
   echo "To avoid being prompted again, run:"
   echo "    export SECRETHUB_USERNAME=your_username"
-  echo "in the terminal."
+  echo "in the terminal, after this script has finished."
 fi
 
 secrethub run --var "user=$SECRETHUB_USERNAME" ./scripts/import-prescription-data-to-mongodb.sh
